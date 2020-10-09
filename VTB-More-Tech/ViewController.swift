6c2a9deb31b92383f39f4ec39eed4b3371bbc7e7
Musa Deniev <m.deniev@zedplus.ru>
2020-10-09 17:49:29 +0300

    Initial Commit

---
+++ VTB-More-Tech/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VTB-More-Tech
+//
+//  Created by admin on 09.10.2020.
+//  Copyright © 2020 TechBirds. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
